0373236878c9084ceff8161aa475991f9757a411
Artur Freitas <arturfreitasgh@gmail.com>
2024-09-05 23:32:41 -0300

    Atualizando

--- Clinica/Consulta.java
+++ Clinica/Consulta.java
@@ -1,4 +1,59 @@
 package Clinica;
+
 public class Consulta {
-    
+    private int id;
+    private String paciente;
+    private String medico;
+    private String data;
+    private String descricao;
+
+    // Construtor
+    public Consulta(int id, String paciente, String medico, String data, String descricao) {
+        this.id = id;
+        this.paciente = paciente;
+        this.medico = medico;
+        this.data = data;
+        this.descricao = descricao;
+    }
+
+    // Getters e Setters
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getPaciente() {
+        return paciente;
+    }
+
+    public void setPaciente(String paciente) {
+        this.paciente = paciente;
+    }
+
+    public String getMedico() {
+        return medico;
+    }
+
+    public void setMedico(String medico) {
+        this.medico = medico;
+    }
+
+    public String getData() {
+        return data;
+    }
+
+    public void setData(String data) {
+        this.data = data;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
 }
